9ed7dda636cdbf68930836dc33ebb2decec6c8da
pequaltonp <bibab737@gmail.com>
2023-02-14 23:52:13 +0600

    database for shop properties added

---
+++ src/main/java/my/project/goods_parser/entity/EShopPropertyEntity.java
@@ -0,0 +1,20 @@
+package my.project.goods_parser.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "e-shop_property")
+public class EShopPropertyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+}
